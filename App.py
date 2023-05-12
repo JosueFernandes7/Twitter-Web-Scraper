e51595972c5157e71a77f7e56a05fb521ecb40aa
Josue Fernandes <josue7mf@hotmail.com>
2023-05-12 01:03:31 -0300

    retornando api

--- App.py
+++ App.py
@@ -9,39 +9,32 @@ from Usuario import Usuario
 
 url = "https://twitter.com/"
 usuario = "diojorange"
-driver = webdriver.Chrome()
-driver.get(f"{url}{usuario}")
-time.sleep(5)
-soup = BeautifulSoup(driver.page_source, features="html.parser")
-# Pegar dados
-arroba = soup.find('div',class_="css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-1qd0xha r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0").text # ok
-nome = soup.find('div',class_="css-901oao r-1awozwy r-18jsvk2 r-6koalj r-1qd0xha r-adyw6z r-1vr29t4 r-135wba7 r-bcqeeo r-1udh08x r-qvutc0").text # ok
-tweets = soup.find('div',class_="css-901oao css-1hf3ou5 r-14j79pv r-1qd0xha r-n6v787 r-16dba41 r-1cwl3u0 r-bcqeeo r-qvutc0").text # ok
-seguidores = soup.find('a', {'href' : '/diojorange/followers', 'class' : 'css-4rbku5 css-18t94o4 css-901oao r-18jsvk2 r-1loqt21 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0'}).text.replace('Seguidores','').strip() #ok
-seguindo = soup.find('span',class_="css-901oao css-16my406 r-18jsvk2 r-poiln3 r-1b43r93 r-b88u0q r-1cwl3u0 r-bcqeeo r-qvutc0").text # ok
 
-usuario = Usuario(arroba,nome,tweets,seguidores,seguindo)
-print(usuario)
-
-
-
-
+def fazerBusca(usuario):
+    driver = webdriver.Chrome()
+    driver.get(f"{url}{usuario}")
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, features="html.parser")
+    # Pegar dados
+    arroba = soup.find('div',class_="css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-1qd0xha r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0").text # ok
+    nome = soup.find('div',class_="css-901oao r-1awozwy r-18jsvk2 r-6koalj r-1qd0xha r-adyw6z r-1vr29t4 r-135wba7 r-bcqeeo r-1udh08x r-qvutc0").text # ok
+    tweets = soup.find('div',class_="css-901oao css-1hf3ou5 r-14j79pv r-1qd0xha r-n6v787 r-16dba41 r-1cwl3u0 r-bcqeeo r-qvutc0").text # ok
+    arrobaFormatado = f"/{arroba.replace('@',' ').strip()}/followers"
+    seguidores = soup.find('a', {'href' : arrobaFormatado, 'class' : 'css-4rbku5 css-18t94o4 css-901oao r-18jsvk2 r-1loqt21 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0'}).text #ok
+    seguindo = soup.find('span',class_="css-901oao css-16my406 r-18jsvk2 r-poiln3 r-1b43r93 r-b88u0q r-1cwl3u0 r-bcqeeo r-qvutc0").text # ok
+    novoUsuario = Usuario(arroba,nome,tweets,seguidores,seguindo)
+    return novoUsuario
 
 
 
 
-
-
-
-# app = Flask(__name__)
-# @app.route('/')
-# def homepage():
-#     return 'Hello World!'
-# @app.route('/usuarios')
-# def iniciar():
-#   result = {}
-#   for url in listaUsuarios:
-#      usuario = scrapper.iniciarBusca(url)
-#      result.__setitem__(url, usuario)
-#   return jsonify(result)
-# app.run()
+app = Flask(__name__)
+@app.route('/')
+def homepage():
+    usuario = fazerBusca("diojorange")
+    print(usuario)
+    return jsonify(usuario.data())
+@app.route('/usuarios')
+def teste():
+    return "Salve"
+app.run()
--- Usuario.py
+++ Usuario.py
@@ -7,3 +7,10 @@ class Usuario:
         self.seguindo = seguindo
     def __str__(self):
        return f"Nome da Página: {self.nome}\nTweets: {self.tweets}\nSeguidores: {self.seguidores}\nSeguindo: {self.seguindo}\nLink:https://twitter.com/{self.arroba}"
+    def data(self):
+        return {'Nome da Página' : self.nome,
+                'Tweets' : self.tweets,
+                'Seguidores' : self.seguidores,
+                'Seguindo' : self.seguindo,
+                'Link' : f'https://twitter.com/{self.arroba}'
+                }
